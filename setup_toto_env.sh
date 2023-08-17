294d006c78d10fdb9e0a88335a574fab17fb9e31
Mohan Kumar S <mohanksriram@gmail.com>
2023-08-17 19:21:10 -0400

    Update setup_toto_env.sh

--- setup_toto_env.sh
+++ setup_toto_env.sh
@@ -10,3 +10,4 @@ conda activate toto-benchmark
 mamba install -y pytorch==1.11.0 torchvision==0.12.0 torchaudio==0.11.0 cudatoolkit=11.3 -c pytorch
 pip install git+https://github.com/AGI-Labs/robot_baselines.git
 pip install git+https://github.com/openai/CLIP.git
+pip install -e .
